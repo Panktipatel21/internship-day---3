7157c7ee449d1cbdfeddbb92f75087291bda46ba
pankti patel <84764279+Panktipatel21@users.noreply.github.com>
2021-05-27 18:53:01 +0530

    day 3
    registration form and display data in next page

---
+++ display.php
@@ -0,0 +1,15 @@
+<?php
+
+echo " <h1> DISPLAY DATA </h1>";
+
+$a = $_POST['txt1'];
+$b = $_POST['txt2'];
+$c = $_POST['txt3'];
+$d = $_POST['txt4'];
+
+echo "<br/> first name  $a";
+echo " <br/>last name  $b";
+echo "<br/>  email id  $c";
+echo " <br/> password  $d";
+
+?>
---
+++ registrationform.php
@@ -0,0 +1,28 @@
+<h1> REGISTRATION FORM  </h1>
+<form action="display.php" method="POST">
+
+	<table>
+		<tr>
+			<td>FIRST NAME:</td>
+			<td><input type="text" name="txt1" required /></td>
+		</tr>
+		
+		<tr>
+			<td>LAST NAME:</td>
+			<td><input type="text" name="txt2" required /></td>
+		</tr>
+		
+		<tr>
+			<td>EMAIL ID:</td>
+			<td><input type="text" name="txt3" required /></td>
+		</tr>
+		
+		<tr>
+			<td>PASSWORD:</td>
+			<td><input type="text" name="txt4" required /></td>
+		</tr>
+		
+		<tr>
+			<td><input type="submit" /></td>
+			<td><input type="reset" /></td>
+	
