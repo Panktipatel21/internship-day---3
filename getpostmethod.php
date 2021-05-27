517ff8f0611ed676d8a988a0038f53be34fdda32
pankti patel <84764279+Panktipatel21@users.noreply.github.com>
2021-05-27 18:51:37 +0530

    day 3
    get and post method program

---
+++ getpostmethod.php
@@ -0,0 +1,17 @@
+
+<h1> FORM DEMO </h1>
+<form action="process.php" method="POST">
+	<table>
+		<tr>
+			<td>USERNAME</td>
+			<td><input type="text" name="txt1" required /></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<td><input type="text" name="txt2" required /></td>
+		</tr>
+		
+		<tr>
+			<td><input type="submit" /></td>
+			<td><input type="reset" /></td>
+	
---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = $_POST['txt1'];
+$b = $_POST['txt2'];
+
+echo " Hi my username is $a and my password is $b";
+
+
+?>
